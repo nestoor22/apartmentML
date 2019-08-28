0b133aa183eb2d6facd4607c3c19b455011c430b
= <=>
2019-08-29 00:17:34 +0300

    Implement OneHotEncoding

--- ml_system.py
+++ ml_system.py
@@ -2,7 +2,7 @@ import sqlite3
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from sklearn.preprocessing import MinMaxScaler, LabelEncoder
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder, LabelEncoder
 from sklearn.model_selection import train_test_split
 from tensorflow import keras
 from keras.models import Sequential
@@ -35,27 +35,30 @@ def scale_numeric_columns(dataset):
     for column in dataset:
         numeric_transformer = MinMaxScaler(feature_range=(0, 1))
         dataset[column] = numeric_transformer.fit_transform(dataset[column].values.reshape(-1, 1))
-        information_about_transformers[column] = {'transformer-object': [numeric_transformer]}
+        information_about_transformers[column] = {'transformer-object': numeric_transformer}
     return dataset
 
 
 def scale_label_columns(dataset):
 
     for column in dataset:
-        string_transformer = LabelEncoder()
-        numeric_transformer = MinMaxScaler(feature_range=(0, 1))
+        label_to_number_transformer = LabelEncoder()
+        one_hot_transformer = OneHotEncoder()
+        labels_number = label_to_number_transformer.fit_transform(dataset[column].values.reshape(-1, 1))
+        labels_to_binary = one_hot_transformer.fit_transform(labels_number.reshape(-1, 1)).toarray()
+        one_hot_dataset = pd.DataFrame(labels_to_binary, columns=[column+'_'+str(int(i))
+                                                                  for i in range(labels_to_binary.shape[1])])
 
-        labels_to_numbers = string_transformer.fit_transform(dataset[column].values.reshape(-1, 1))
-        numbers_to_range = numeric_transformer.fit_transform(labels_to_numbers.reshape(-1, 1))
-        information_about_transformers[column] = {'transformer-object': [numeric_transformer, string_transformer]}
-        dataset[column] = numbers_to_range
+        information_about_transformers[column] = {'transformer-object': one_hot_transformer}
+        dataset = pd.concat([dataset, one_hot_dataset], axis=1)
 
+    print(dataset.columns)
     return dataset
 
 
 def decode_numeric(dataset, columns):
     for column in columns:
-        dataset[column] = information_about_transformers[column]['transformer-object'][0].\
+        dataset[column] = information_about_transformers[column]['transformer-object'].\
             inverse_transform(dataset[column].values.reshape(-1, 1))
 
     return dataset
@@ -63,13 +66,10 @@ def decode_numeric(dataset, columns):
 
 def decode_labels(dataset, columns):
     for column in columns:
-        number_of_label = information_about_transformers[column]['transformer-object'][0].\
+        binary_to_label = information_about_transformers[column]['transformer-object'].\
             inverse_transform(dataset[column].values.reshape(-1, 1))
 
-        label_string = information_about_transformers[column]['transformer-object'][1].\
-            inverse_transform(number_of_label.reshape(len(number_of_label),).astype(int))
-
-        dataset[column] = label_string
+        dataset[column] = binary_to_label
 
     return dataset
 
@@ -82,7 +82,7 @@ def rescale_data(dataset):
     dataset_with_numeric_columns = scale_numeric_columns(original_dataset[columns_with_numeric_data])
 
     rescaled_dataset = pd.concat([dataset_with_numeric_columns, dataset_with_label_columns], axis=1)
-
+    print(rescaled_dataset.columns)
     # decode_numeric(rescaled_dataset, columns_with_numeric_data)
     # decode_labels(rescaled_dataset, columns_with_str_data)
 
