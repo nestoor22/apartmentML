323c196708cab379c11f5f8c1943911f46662cca
nestoor22 <nestorslavko45@gmail.com>
2019-10-19 15:30:28 +0300

    Add Spider for scrapping link with information about apartments in Kyiv

--- scrap_data.py
+++ scrap_data.py
@@ -1,5 +1,5 @@
 import os
 
 def run_spiders():
-    os.system('scrapy crawl apartment_links_in_lviv -o lviv_pages_link.json && scrapy crawl get_apartment_info_in_lviv -o lviv_info.json')
+    os.system('scrapy crawl apartments_links_in_lviv -o lviv_pages_link.json && scrapy crawl get_apartment_info_in_lviv -o lviv_info.json')
 
--- spiders/spiders/pages_spider.py
+++ spiders/spiders/pages_spider.py
@@ -2,7 +2,7 @@ import scrapy
 
 
 class LvivPageSpider(scrapy.Spider):
-    name = "apartment_links_in_lviv"
+    name = "apartments_links_in_lviv"
 
     def start_requests(self):
         urls = [
@@ -13,12 +13,32 @@ class LvivPageSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        for quote in response.css('li.page'):
-            if quote.css('a[rel="next"]::attr(href)').get() is not None:
-                link = 'https://www.real-estate.lviv.ua' + quote.css('a[rel="next"]::attr(href)').get()
+        for page_link in response.css('li.page'):
+            if page_link.css('a[rel="next"]::attr(href)').get() is not None:
+                link = 'https://www.real-estate.lviv.ua' + page_link.css('a[rel="next"]::attr(href)').get()
                 yield {'link': link}
 
         next_page = response.css('li.next a::attr(href)').get()
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
+
+
+class KyivPageScrapper(scrapy.Spider):
+    name = 'apartments_links_in_kyiv'
+
+    def start_requests(self):
+        urls = ['https://100realty.ua/uk/realty_search/apartment/sale/cur_3']
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        i = 0
+        for page_link in response.css('li[class="pager-item pager-next"]'):
+            if page_link.css('a[rel="next"]::attr(href)').get() is not None and not i:
+                i = 1
+                yield {'link': page_link.css('a[rel="next"]::attr(href)').get()}
+        next_page = response.css('li[class="pager-item pager-next"] a::attr(href)')
+        if next_page is not None:
+            yield scrapy.Request(url=next_page.get(), callback=self.parse)
